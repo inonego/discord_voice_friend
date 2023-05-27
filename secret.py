c5dc3b77f3ff551b60516d63eee049d8e9277061
inonego <hmud0721@naver.com>
2023-05-27 16:53:32 +0900

    Added secret.py

---
+++ secret.py
@@ -0,0 +1,18 @@
+OPENAI_API_KEY = 'YOUR_OPEN_API_KEY'
+DISCORD_TOKEN = 'YOUR_DISCORD_TOKEN'
+
+prompt = '''
+프롬프트를 입력하시면 됩니다.
+'''
+
+noise_word = [
+' 시청해주셔서 감사합니다.',
+' 감사합니다.',
+' 안녕히계세요',
+' 지금까지 재택 플러스였습니다.',
+' MBC 뉴스 이덕영입니다.',
+' 고맙습니다.',
+' 그럼 안녕'
+] 
+
+failed_comment = "OPEN AI API 로드에 실패했습니다."
